d01afa83b84af222b6182dcf99f3ad8727ce7a19
GSCrawley <gscrawley@gmail.com>
2019-11-21 15:42:19 -0800

    attack

--- superheroes.py
+++ superheroes.py
@@ -27,9 +27,41 @@ class Hero:
         self.armors = list()
         self.name = name
         self.starting_health = starting_health
-        self.current_health = starting_health    
+        self.current_health = starting_health   
         
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+    
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+#     def add_armor(self, armor):
+#         self.armors.append(armor)
+
+#     def defend(self):
+#         total_block = 0
+#         for armor in self.armors:
+#             total_block += armor.block()
+#         return total_block
+
+      # TODO: This method should run Ability.attack() on every ability
+      # in self.abilities and returns the total as an integer.
+
+#
+#     def defend(self, incoming_damage):
+#
+#     def take_damage(self, damage):
+#
+#     def is_alive(self):
+#
+#     def fight(self, opponent):
 if __name__ == "__main__":
-        my_hero = Hero("Grace Hopper", 200)
-        print(my_hero.name)
-        print(my_hero.current_health)
+        ability = Ability("Great Debugging", 50)
+        another_ability = Ability("Smarty Pants", 90)
+        hero = Hero("Grace Hopper", 200)
+        hero.add_ability(ability)
+        hero.add_ability(another_ability)
+        print(hero.attack())
