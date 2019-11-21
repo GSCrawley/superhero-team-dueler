2336d7250f3b65cfbf39371523bdc6cb61abd588
GSCrawley <gscrawley@gmail.com>
2019-11-21 14:45:28 -0800

    ability class

--- superheroes.py
+++ superheroes.py
@@ -2,55 +2,55 @@ import random
 
 class Ability:
 
-    def __init__(self, name, max_damage):
+    def __init__(self, name, attack_strength):
         self.name = name
-        self.max_damage = max_damage
+        self.attack_strength = attack_strength
 
     def attack(self):
-        rand_hit = random.randint(0,self.max_damage)
+        rand_hit = random.randint(0,self.attack_strength)
         return rand_hit
 
         ''' Return a value between 0 and the value set by self.max_damage.'''
 
-class Armor:
-    def __init__(self, name, max_block):
-        self.name = name
-        self.max_block = max_block
+# class Armor:
+#     def __init__(self, name, max_block):
+#         self.name = name
+#         self.max_block = max_block
 
-    def block(self):
-        ''' Return a random value between 0 and the initialized max_block strength. '''
-        return random.randint(0,max_block)
+    # def block(self):
+    #     ''' Return a random value between 0 and the initialized max_block strength. '''
+    #     return random.randint(0,max_block)
 
-class Hero:
-    def __init__(self, name, starting_health=100):
-        self.name = name
-        self.starting_health = starting_health
-        self.current_health = starting_health
-        self.abilities = []
-        self.armors = []
-#     self.deaths = 0
-#         # self.kills = 0
-#
-    def add_ability(self, ability):
-        ''' Add ability to abilities list '''
-        self.abilities.append(ability)
+# class Hero:
+#     def __init__(self, name, starting_health=100):
+#         self.name = name
+#         self.starting_health = starting_health
+#         self.current_health = starting_health
+#         self.abilities = []
+#         self.armors = []
+# #     self.deaths = 0
+# #         # self.kills = 0
+# #
+#     def add_ability(self, ability):
+#         ''' Add ability to abilities list '''
+#         self.abilities.append(ability)
 
-    def attack(self):
-        '''Calculate the total damage from all ability attacks.
-        return: total:Int'''
-        total_damage = 0
-        for ability in self.abilities:
-            total_damage += ability.attack()
-        return total_damage
+#     def attack(self):
+#         '''Calculate the total damage from all ability attacks.
+#         return: total:Int'''
+#         total_damage = 0
+#         for ability in self.abilities:
+#             total_damage += ability.attack()
+#         return total_damage
 
-    def add_armor(self, armor):
-        self.armors.append(armor)
+#     def add_armor(self, armor):
+#         self.armors.append(armor)
 
-    def defend(self):
-        total_block = 0
-        for armor in self.armors:
-            total_block += armor.block()
-        return total_block
+#     def defend(self):
+#         total_block = 0
+#         for armor in self.armors:
+#             total_block += armor.block()
+#         return total_block
 
       # TODO: This method should run Ability.attack() on every ability
       # in self.abilities and returns the total as an integer.
@@ -67,9 +67,10 @@ class Hero:
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block of code is executed.
-    ability = Ability("Great Debugging", 50)
-    another_ability = Ability("Smarty Pants", 90)
-    hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    hero.add_ability(another_ability)
-    print(hero.attack())
+    ability = Ability("Great Debugging", 20)
+    # another_ability = Ability("Smarty Pants", 90)
+    # hero = Hero("Grace Hopper", 200)
+    # hero.add_ability(ability)
+    # hero.add_ability(another_ability)
+    print(ability.name)
+    print(ability.attack())
